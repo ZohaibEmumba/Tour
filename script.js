66a4d8b2f53afab8c50d9a6c1aa0b9065f8bb3c6
Zohaibkhattak15 <zohaibkhattak6@gmail.com>
2022-02-07 11:54:06 +0500

    added changes

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const toggler = document.querySelector(".navbar > .toggler"),
   navListContainer = document.querySelector(".navbar > .nav-list-container");
+const flight = document.getElementById("Flight");
+const hotel = document.getElementById("Hotel");
+const car = document.getElementById("Car");
+const crusies = document.getElementById("Crusies");
+const footer = document.getElementById("footer");
+const controller = document.getElementById("controller");
 
 toggler.addEventListener(
   "click",
@@ -41,74 +47,20 @@ function showSlides(n) {
   dots[slideIndex - 1].className += " active";
 }
 
-// let prevPageName = "";
-
-// function openPage(pageName, elmnt, color) {
-//   var i, tabcontent, tablinks;
-
-//   //this is doing the display none for all the tabs one by one
-//   tabcontent = document.getElementsByClassName("tabcontent");
-//   const Flight=  document.getElementById("Flight");
-
-//   console.log(Flight);
-
-//   for (i = 0; i < tabcontent.length; i++) {
-//     tabcontent[i].style.display = "none";
-//   }
-//   // this is making the color of all to empty
-//   tablinks = document.getElementsByClassName("tablink");
-//   for (i = 0; i < tablinks.length; i++) {
-//     tablinks[i].style.backgroundColor = "";
-//   }
-
-//   if (pageName !== prevPageName) {
-//     // document.getElementById(pageName).style.display = "block";
-//     // document.getElementById(pageName).style.width = "97.9%";
-//     Flight.classList.toggle('active');
-//     elmnt.style.backgroundColor = color;
-//   }
-//     prevPageName = pageName;
-// }
-
-// let flight = document.getElementById("Flight");
-// let hotel = document.getElementById("Hotel");
-// let car = document.getElementById("Car");
-// let crusies = document.getElementById("Crusies");
-
 document.querySelectorAll("button").forEach((btn) => {
   btn.addEventListener("click", () => {
-//     const markUp = `
-//       <div id="${btn.innerText}" class="tabcontent active">
-//       <h3>${btn.innerText}</h3>
-//       <p>${btn.innerText} is where the heart is..</p>
-//       </div>
-// console.log(`${btn.innerText}`);
-// `;
+    btn.classList.toggle("_active");
+    footer.classList.toggle("_active");
+    controller.classList.toggle("dotactive");
 
-if(`${btn.innerText}`){
-  console.log(`${btn.innerText}`);
-    btn.classList.toggle('_active');
-    document.getElementById("Flight").style.marginLeft = "0px";
-    document.getElementById("Flight").style.width = "100%";
-  }
-  else{
-    btn.classList.remove('_active');
-    document.getElementById("Flight").style.marginLeft = "214px";
-    document.getElementById("Flight").style.width = "0%";
-  }
+    if (`${btn.innerText}` === "Flight") {
+      flight.classList.toggle("tabactive");
+    } else if (`${btn.innerText}` === "Hotel") {
+      hotel.classList.toggle("tabactive");
+    } else if (`${btn.innerText}` === "Car") {
+      car.classList.toggle("tabactive");
+    } else {
+      crusies.classList.toggle("tabactive");
+    }
   });
 });
-
-function openPage() {
-  // flight.classList.remove('tabcontent')
-  // flightBtn.classList.toggle('_active');
-  // console.log(flight);
-}
-
-//  }
-// else if (`${btn.innerText}` === "Hotel"){
-//   btn.classList.toggle('_active');
-//   hotel.classList.toggle('active');
-//   flight.classList.remove('active');
-// }
-// else null
